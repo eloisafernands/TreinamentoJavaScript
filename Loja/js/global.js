f21255804fad1099b65ec3b88ca7e0b84a441a14
Eloisa Fernandes <eloisa.fernands@gmail.com>
2022-05-01 11:27:11 -0300

    App de loja

---
+++ Loja/js/global.js
@@ -0,0 +1,19 @@
+/*
+        EXERCICIO 1
+
+    CRIAR DUAS CLASSES
+    CLASSE 1: Carrinho.
+    Carrinho terá os seguintes objeto:
+        produtos;
+    Métodos e inserir os comentários: 
+        atualizarCarrinho; // atualizar carrinho html
+        removerProduto;  // remover produto db
+        adicionarProduto; // adicionar produto db
+    
+    CLASSE 2: Global
+        construtor() // vazio 
+
+    HTML - Criar uma página de cadastrar produtos.
+    Campos: nome, preço, imagem, quantidade, descricao.
+
+*/
